3b2ae267ebb4587ead346e3fb8b39d5ba6476266
Lucas Toledo <lucasmoraisss@gmail.com>
2022-06-28 07:30:29 -0300

    init: starting project

---
+++ src/test/java/io/github/lucasmorais/sensorhub/SensorhubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucasmorais.sensorhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
